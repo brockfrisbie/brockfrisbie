94a21b946d2696f8db703b317666573e381c1fe7
Brock <brockfrisbie@gmail.com>
2025-03-13 15:59:24 +0000

    Created a layout template and the index page

--- brockfrisbie/brockfrisbie/urls.py
+++ brockfrisbie/brockfrisbie/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from . import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path('', views.index),
     path("bitcoin/", include("bitcoin.urls")),
+     
 ]
